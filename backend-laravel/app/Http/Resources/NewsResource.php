8e87564a5bcc47585a2fbfd719c6b282a2b649f6
Buddyy <singgihbudi.sbh@gmail.com>
2024-01-13 21:08:38 +0700

    add climate news_rev

---
+++ backend-laravel/app/Http/Resources/NewsResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'url' => $this->url,
+            'published_at' => $this->published_at,
+            'image_url' => $this->image_url,
+        ];
+    }
+}
