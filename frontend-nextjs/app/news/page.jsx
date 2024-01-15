a6bda91fad86df3063166891c052eb55ea6bc18b
Maulana Dimyati <dimyatimaulana@gmail.com>
2024-01-15 17:10:02 +0700

    feat: use react paginate library

--- frontend-nextjs/app/news/page.jsx
+++ frontend-nextjs/app/news/page.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import MainLayout from "@/layouts/MainLayout";
-import NewsCardsList from "@/components/NewsCardList";
+import NewsList from "@/components/NewsList";
 
 import Image from "next/image";
 import newsHero from "../../public/images/energi-bersih.webp";
 
-import { FaSearch } from "react-icons/fa";
 
 export const metadata = {
   title: "News",
@@ -27,32 +26,7 @@ const page = () => {
           <h2 className="font-bold text-4xl py-1 md:text-5xl">News</h2>
         </div>
       </section>
-      <section
-        id="section-2"
-        className="bg-white flex items-center justify-center h-300px"
-      >
-        <div className="grid">
-          <div className="join">
-            <div>
-              <input
-                className="input input-bordered join-item"
-                placeholder="Search"
-              />
-            </div>
-            <div className="indicator">
-              <span className="indicator-item badge badge-secondary">new</span>
-              <button className="btn join-item text-xl bg-black text-white">
-                <FaSearch />
-              </button>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section id="section-3" className="bg-white">
-        <div className="grid p-6 gap-5 md:max-w-screen-lg">
-          <NewsCardsList />
-        </div>
-      </section>
+      <NewsList />
     </MainLayout>
   );
 };
---
+++ frontend-nextjs/components/NewsList.jsx
@@ -0,0 +1,117 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NewsCard from "./NewsCard.";
+import ReactPaginate from "react-paginate";
+
+import { FaSearch } from "react-icons/fa";
+
+const NewsList = () => {
+  const [news, setNews] = useState([]);
+  const [query, setQuery] = useState("");
+  const titleQuery = query.toLowerCase();
+  const dotenv = require("dotenv");
+  dotenv.config();
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY || "";
+  const API_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
+  const NEWS_API = `${API_URL}${API_KEY}`;
+
+  const getNews = async () => {
+    const response = await axios.get(`${NEWS_API}`);
+    const responseJson = await response.data.articles;
+    setNews(responseJson);
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getFilteredNews = (query, items) => {
+    if (!query) {
+      return items;
+    }
+    return items.filter((news) => news.title.toLowerCase().includes(query));
+  };
+
+  const filteredNews = getFilteredNews(titleQuery, news);
+
+  const PaginatedItem = ({ itemsPerPage }) => {
+    const [itemOffset, setItemOffset] = useState(0);
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = filteredNews.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(filteredNews.length / itemsPerPage);
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % filteredNews.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    };
+
+    return (
+      <>
+        {currentItems.map((news, idx) => {
+          return (
+            <NewsCard
+              key={idx}
+              urlImages={news.image}
+              urlNews={news.url}
+              title={news.title}
+              description={news.description}
+              publishedAt={news.publishedAt}
+            />
+          );
+        })}
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          className="flex flex-wrap gap-2 justify-center font-bold md:max-w-screen-md m-auto"
+          pageClassName="flex-1 px-4 py-2 rounded-md page-number"
+          previousClassName="flex-1 px-4 py-2 page-number"
+          nextClassName="flex-1 px-4 py-2 page-number"
+          activeClassName="text-black border-2 border-black"
+        />
+      </>
+    );
+  };
+
+  return (
+    <>
+      <section
+        id="section-2"
+        className="bg-white flex items-center justify-center h-300px"
+      >
+        <div className="grid">
+          <div className="join">
+            <div>
+              <input
+                className="input input-bordered join-item"
+                placeholder="Search"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+            <div className="indicator">
+              {/* <span className="indicator-item badge badge-secondary">new</span> */}
+              <button className="btn join-item text-xl bg-black text-white">
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="section-3" className="bg-white">
+        <div className="grid p-6 gap-5 md:max-w-screen-lg m-auto">
+          <PaginatedItem itemsPerPage={5} />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default NewsList;
